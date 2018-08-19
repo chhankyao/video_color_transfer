2155700c6d8a241a31b7c04fed391f266b33d83d
Chun-Han Yao <hank@UNKNOWN-172-132-38-X.yahoo.com>
2018-08-19 15:45:52 -0700

    code updated

--- myProcess.m
+++ myProcess.m
@@ -1,13 +1,11 @@
 
-v = VideoReader('input2.avi');
-% frames = imresize(read(v, [520 620]), 0.7); % [2 100] [520 620] [630 730]
+v = VideoReader('input.avi');
 frames = read(v);
-ref = imread('ref2.jpg');
+ref = imread('ref.jpg');
 
 output = zeros(size(frames));
 prev_frame = frames(:,:,:,1);
 prev_output = im2uint8(fusion(frames(:,:,:,1), frames(:,:,:,1), ref, ones(size(frames(:,:,:,1)))));
-% prev_output = im2uint8(gradientPreserving(frames(:,:,:,1), ref));
 verb = '';
 for i = 1 : size(frames, 4)
     fprintf(repmat('\b',[1, length(verb)]))
@@ -16,22 +14,18 @@ for i = 1 : size(frames, 4)
     current_frame = frames(:,:,:,i);
     ann_i = nnmex(current_frame, prev_frame, 'cpu', [], [], [], [], [], [], 1);
     pmatch = votemex(prev_output, ann_i);
-    % ssim_i = ssim(current_frame, prev_frame);
     ann = double(ann_i(1:end-6, 1:end-6, 3));
     w = double(ann_i(:,:,3)) / (max(ann(:))+1e-10);
     w(w > 1) = 1;
     w = repmat(w, [1,1,3]);
     % style transfer
     trans = im2uint8(fusion(current_frame, pmatch, ref, w));
-    % trans = im2uint8(fusion(current_frame, current_frame, ref, 0));
-%     trans = im2uint8(gradientPreserving(current_frame, ref));
     output(:,:,:,i) = im2double(trans);
     prev_frame = current_frame;
     prev_output = trans;
 end
 fprintf(repmat('\b',[1, length(verb)]))
 
-% implay(output);
 v = VideoWriter('output4-7-3.avi');
 open(v);
 writeVideo(v, output);
--- myTest.m
+++ myTest.m
@@ -1,8 +1,6 @@
 
-vi = VideoReader('input2.avi');
-vo = VideoReader('output4-7-5.avi');
-%vw = VideoWriter('error6-6.avi');
-%open(vw);
+vi = VideoReader('input.avi');
+vo = VideoReader('output.avi');
 
 n = 0;
 s = zeros(1, 31);
@@ -20,18 +18,16 @@ while (hasFrame(vi) && hasFrame(vo))
         mse_i = (pmatch_i - fi) .^ 2;
         mse_o = (pmatch_o - fo) .^ 2;
         tcm = abs(mse_i-mse_o) ./ mse_i;
-        %f = im2uint8(tcm);
-        %f(tcm < 50) = 255;
-        %f(tcm >= 50) = 0;
-        %writeVideo(vw, f);
-        %mse_i = immse(pmatch_i, fi);
-        %mse_o = immse(pmatch_o, fo);
-        %s(n) = mse_o / mse_i;
+        f = im2uint8(tcm);
+        f(tcm < 50) = 255;
+        f(tcm >= 50) = 0;
+        writeVideo(vw, f);
+        mse_i = immse(pmatch_i, fi);
+        mse_o = immse(pmatch_o, fo);
+        s(n) = mse_o / mse_i;
     end
     prev_fi = fi;
     prev_fo = fo;
     n = n + 1;
 end
 fprintf(repmat('\b',[1, length(verb)]));
-%close(vw);
-s2 = sum(s) / (n-1);
